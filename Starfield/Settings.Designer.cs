5e4d112c35f7c4034c4ec5e6a271d6c4ea7b9eff
Klementiev Daniil <Klem_em31@student.itstep.org>
2022-04-22 17:53:39 +0300

    Add project files.

---
+++ Starfield/Settings.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Starfield
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Количество звезд: ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10000",
+            "15000",
+            "20000",
+            "30000",
+            "50000",
+            "100000"});
+            this.comboBox1.Location = new System.Drawing.Point(237, 40);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 40);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Цвет:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Red",
+            "Yellow",
+            "Purple",
+            "Blue",
+            "Rainbow"});
+            this.comboBox2.Location = new System.Drawing.Point(237, 88);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(213, 40);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Commit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 198);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private ComboBox comboBox2;
+        private Button button1;
+    }
+}
---
+++ Starfield/Settings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unity;
+
+namespace Starfield
+{
+    public partial class Settings : Form
+    {
+        private int starsCount;
+        private Brush color;
+        public static UnityContainer Container { get; set; }
+        public Settings()
+        {
+            InitializeComponent();
+            starsCount = 10000;
+            color = Brushes.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Container = new UnityContainer();
+                  
+            
+            starsCount = int.Parse(comboBox1.Text);
+            if(comboBox2.SelectedIndex == 0)
+            {
+                color = Brushes.Red;
+            }
+            else if(comboBox2.SelectedIndex == 1)
+            {
+                color = Brushes.Yellow;
+            }
+            else if(comboBox2.SelectedIndex == 2)
+            {
+                color = Brushes.Purple;
+            }
+            else if(comboBox2.SelectedIndex == 3)
+            {
+                color = Brushes.Blue;
+            }
+            else if (comboBox2.SelectedIndex == 4)
+            {
+                color = Brushes.Plum;
+            }
+            Container.RegisterInstance(starsCount);
+            Container.RegisterInstance(color);
+            Container.Resolve<Form1>().Show();
+        }
+    }
+}
